921342d84bac92a81315ca1805e8b70db7c19999
Abhimanyu Yadav <yabhimanyuofficial@gmail.com>
2026-08-16 19:54:01 +0530

    Delete DAY 1 directory

--- DAY 1/band_name_generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the band name Generator Program")
-city = input("Enter the name of the city where you grew up: ")
-pet = input("Enter the name of your pet: ")
-print ("Your band could be " + city + " " + pet + "'s")
